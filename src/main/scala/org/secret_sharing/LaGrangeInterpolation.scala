556a499fe78992c45566233501b8f3631cb17453
437580 <yannick.gladow@holidaycheck.com>
2017-01-13 20:17:19 +0100

    * added case class for gcdD

--- src/main/scala/org/secret_sharing/LaGrangeInterpolation.scala
+++ src/main/scala/org/secret_sharing/LaGrangeInterpolation.scala
@@ -16,7 +16,7 @@ private[secret_sharing] object LaGrangeInterpolation {
     shares.indices.foldLeft(BigInt(0)) { case (sum, i) =>
       val (numerator, denum) = shares.indices.foldLeft(BigInt(1), BigInt(1)) { case ((num, den), j) =>
         if (i != j) (num * (0 - xShares(j))).mod(prime) -> (den * (xShares(i) - xShares(j))).mod(prime)
-        else num -> den
+        else (num, den)
       }
       (prime + sum + yShares(i) * numerator * modInverse(denum, prime)) % prime
     }
@@ -25,18 +25,21 @@ private[secret_sharing] object LaGrangeInterpolation {
   /**
    * see https://en.wikipedia.org/wiki/Shamir's_Secret_Sharing#Javascript_example
    */
- def gcdD(a: BigInt, b: BigInt): List[BigInt] =
-    if(b == 0) List(a, BigInt(1), BigInt(0))
+  def gcdD(a: BigInt, b: BigInt): GcdD =
+    if (b == 0) GcdD(a, BigInt(1), BigInt(0))
     else {
       val div = a / b
       val rest = a % b
       val gcdDecomposed = gcdD(b, rest)
-      List(gcdDecomposed.head, gcdDecomposed(2), gcdDecomposed(1) - gcdDecomposed(2) * div)
+      GcdD(gcdDecomposed.gcd, gcdDecomposed.res, gcdDecomposed.oldRes - gcdDecomposed.res * div)
     }
 
   private def modInverse(k: BigInt, prime: BigInt): BigInt = {
     val m = k % prime
-    val decompositionOfGcd = if(m < 0) -gcdD(prime, -m)(2) else gcdD(prime, m)(2)
+    val decompositionOfGcd = if (m < 0) -gcdD(prime, -m).res else gcdD(prime, m).res
     (prime + decompositionOfGcd) % prime
   }
+
+  case class GcdD(gcd: BigInt, oldRes: BigInt, res: BigInt)
+
 }
