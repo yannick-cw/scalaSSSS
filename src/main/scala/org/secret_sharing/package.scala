7313c5f6bc721cc5547965ea30a28ee4b540eafb
437580 <yannick.gladow@holidaycheck.com>
2017-01-13 19:50:20 +0100

    * swoop

--- src/main/scala/org/secret_sharing/package.scala
+++ src/main/scala/org/secret_sharing/package.scala
@@ -10,12 +10,12 @@ package object secret_sharing {
   implicit def eitherToRight[A, B](either: Either[A, B]): RightProjection[A, B] = either.right
 
   implicit def eitherMonad[Err] = new Monad[Either[Err, ?]]{
-    override def flatMap[A, B](fa: Either[Err, A])(f: (A) => Either[Err, B]): Either[Err, B] = fa.right.flatMap(f)
-    override def tailRecM[A, B](a: A)(f: (A) => Either[Err, Either[A, B]]): Either[Err, B] = f(a) match {
+    def flatMap[A, B](fa: Either[Err, A])(f: (A) => Either[Err, B]): Either[Err, B] = fa.right.flatMap(f)
+    def tailRecM[A, B](a: A)(f: (A) => Either[Err, Either[A, B]]): Either[Err, B] = f(a) match {
       case Right(Right(b)) => Either.right(b)
       case Right(Left(a)) => tailRecM(a)(f)
       case l@Left(_) => l.rightCast[B]
     }
-    override def pure[A](x: A): Either[Err, A] = Either.right(x)
+    def pure[A](x: A): Either[Err, A] = Either.right(x)
   }
 }
