e8517ebc890f5fee5cb73443a7afdc335fd7aa60
437580 <yannick.gladow@holidaycheck.com>
2017-01-13 19:36:37 +0100

    * trying to generify the SSSSOps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
+  lazy val cats = "org.typelevel" %% "cats" % "0.8.1"
 }
--- src/main/scala/org/secret_sharing/package.scala
+++ src/main/scala/org/secret_sharing/package.scala
@@ -1,8 +1,21 @@
 package org
 
+import cats.Monad
+import cats.implicits._
+
 import scala.util.Either.RightProjection
 
 package object secret_sharing {
   // used for scala 2.11.8 compatibility
   implicit def eitherToRight[A, B](either: Either[A, B]): RightProjection[A, B] = either.right
+
+  implicit def eitherMonad[Err] = new Monad[Either[Err, ?]]{
+    override def flatMap[A, B](fa: Either[Err, A])(f: (A) => Either[Err, B]): Either[Err, B] = fa.right.flatMap(f)
+    override def tailRecM[A, B](a: A)(f: (A) => Either[Err, Either[A, B]]): Either[Err, B] = f(a) match {
+      case Right(Right(b)) => Either.right(b)
+      case Right(Left(a)) => tailRecM(a)(f)
+      case l@Left(_) => l.rightCast[B]
+    }
+    override def pure[A](x: A): Either[Err, A] = Either.right(x)
+  }
 }
